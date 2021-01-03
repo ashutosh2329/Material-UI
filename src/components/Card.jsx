cbf144f677580d39879a29f83172d621b9f4e9ca
ashutosh <sonutiwari2329@gmail.com>
2021-01-03 15:08:15 +0530

    card element created

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,31 +1,113 @@
-import React from "react";
-import { Card, CardContent, Typography } from "@material-ui/core/";
-import { makeStyles } from "@material-ui/styles";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { blue } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import image from '../static/images/cardImage.png'
+import { Grid } from '@material-ui/core';
 
-const usestyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
-    minWidth: 275,
+    maxWidth: 400,
+    borderRadius: 20,
+    
   },
-  title: {
-    fontSize: 20,
-    padding: (5, 0, 5, 20),
+  grid: {
+    paddingTop: '5%', 
   },
-});
-function simpleCard() {
-  const classes = usestyles();
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: blue[500],
+  },
+}));
+
+export default function RecipeReviewCard() {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
   return (
-    <CardContent>
-      <Card className={classes.root}>
-        <Typography
-          className={classes.title}
-          color="textPrimary"
-          display="inline"
-        >
-          Ashutosh Tiwari
+    <Grid container direction="column" alignContent="center" justify="center" className={classes.grid}>
+    <Card className={classes.root}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="React"
+        subheader="Mention the date"
+      />
+      <CardMedia
+        className={classes.media}
+        image={image}
+        title="Image"
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          React makes it painless to create interactive UIs.Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.
         </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Theory:</Typography>
+          <Typography paragraph>
+            React (also known as React.js or ReactJS) is an open-source, front end, JavaScript library[3] for building user interfaces or UI components. It is maintained by Facebook and a community of individual developers and companies.[4][5][6] React can be used as a base in the development of single-page or mobile applications. However, React is only concerned with state management and rendering that state to the DOM, so creating React applications usually requires the use of additional libraries for routing.[7][8] React Router[9] is an example of such a library.
+          </Typography>
+        </CardContent>
+      </Collapse>
       </Card>
-    </CardContent>
+      </Grid>
   );
 }
-
-export default simpleCard;
